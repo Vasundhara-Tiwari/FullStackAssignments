33db13753f46dc2d53319418592fcb05856263f0
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-10-17 22:17:39 +0530

    added increase function logic to increment the counter

--- scripts/practical14.js
+++ scripts/practical14.js
@@ -1,2 +1,13 @@
 let count = 0;
 let button1, button2;
+function increase() {
+  button1 = document.getElementById("plus-button");
+  var value = document.getElementById("value");
+  button1.onclick = function () {
+    count += 1;
+    value.innerHTML = count;
+    if (button2.disabled == true && count > 0) {
+      button2.disabled = false;
+    }
+  };
+}
